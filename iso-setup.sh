6bc09c7b92b6df57aa255483243d5ae0159e0f57
kenielf <chrystian.keni.github@gmail.com>
2022-01-29 10:16:41 -0300

    Fixed Easter Egg Adition

--- iso-setup.sh
+++ iso-setup.sh
@@ -11,7 +11,7 @@ echo "Configuring pacman."
 sed -i "/^#Color/c\Color" /etc/pacman.conf
 sed -i "/^#VerbosePkgLists/c\VerbosePkgLists" /etc/pacman.conf
 sed -i "/^#ParallelDownloads = .*/c\ParallelDownloads = $cpu_count" /etc/pacman.conf
-sed -i "^/ParallelDownloads = .*/a ILoveCandy" /etc/pacman.conf
+sed -i "/^ParallelDownloads = .*/a ILoveCandy" /etc/pacman.conf
 ## Enable Multilib Repository
 sed -i '/^#\[multilib\]$/c\\[multilib\]' /etc/pacman.conf
 sed -i '/^\[multilib\]$/!b;n;cInclude = \/etc\/pacman\.d\/mirrorlist' /etc/pacman.conf
