45db9780321e55591c77cc279f98dd948187cb08
kenielf <chrystian.keni.github@gmail.com>
2022-01-29 09:39:20 -0300

    Updated iso-setup.sh again

--- iso-setup.sh
+++ iso-setup.sh
@@ -7,14 +7,17 @@ cpu_count=$(lscpu | grep -E "(^CPU\(s\):.*)" | grep -oP "(\d*)$")
 ###--- SCRIPT ---###
 # Configure pacman
 #sed -i "/aaa=/c\aaa=xxx" your_file_here
+echo "Configuring pacman."
 sed -i "/^#Color/c\Color" /etc/pacman.conf
-sed -i "/^#VerbosePkgLists/c/VerbosePkgLists" /etc/pacman.conf
-sed -i "/^#ParallelDownloads = 5/c/ParallelDownloads = $cpu_count" /etc/pacman.conf
+sed -i "/^#VerbosePkgLists/c\VerbosePkgLists" /etc/pacman.conf
+sed -i "/^#ParallelDownloads = 5/c\ParallelDownloads = $cpu_count" /etc/pacman.conf
 
 # Install git on ISO
+echo "Installing git"
 pacman -Sy git
 
 # Download repository and cd into it
+echo "Cloning repo"
 git clone "$repo_url"
 cd $repo_name
 
