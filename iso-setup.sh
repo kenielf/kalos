05e69c3031dfb590f248bbba76c20b27cdd4dfc9
kenielf <chrystian.keni.github@gmail.com>
2022-01-29 09:30:39 -0300

    Updated iso-setup.sh

--- iso-setup.sh
+++ iso-setup.sh
@@ -2,11 +2,14 @@
 ###--- CONSTANTS ---###
 repo_name="kalos"
 repo_url="https://github.com/kenielf/$repo_name"
+cpu_count=$(lscpu | grep -E "(^CPU\(s\):.*)" | grep -oP "(\d*)$")
 
 ###--- SCRIPT ---###
 # Configure pacman
 #sed -i "/aaa=/c\aaa=xxx" your_file_here
-sed -i "/^#Color=/c\Color" /etc/pacman.conf
+sed -i "/^#Color/c\Color" /etc/pacman.conf
+sed -i "/^#VerbosePkgLists/c/VerbosePkgLists" /etc/pacman.conf
+sed -i "/^#ParallelDownloads = 5/c/ParallelDownloads = $cpu_count" /etc/pacman.conf
 
 # Install git on ISO
 pacman -Sy git
